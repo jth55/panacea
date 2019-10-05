3222fa6d826716aaf12ca9e64d9ed219519e1639
543hn.com <20675508+543hn@users.noreply.github.com>
2019-10-05 17:11:53 -0500

    Add Carl data

--- engine/model.py
+++ engine/model.py
@@ -1,15 +1,20 @@
-import logging, datetime, random, copy, json, ast, subprocess, yaml
+import logging, datetime, random, copy, json, ast, subprocess
 import collections, sys, json
 from threading import Thread, Timer
 from hashlib import sha256
 from enum import IntEnum
 from time import sleep
-#from engine import db
+
+
+def get_json(json_file):
+    """Returns json data for specified patient UUID, demo uses test json data """
+    with open("data/patientData0.json", "r") as json_file:
+        return json.load(json_file)
+
 
 class MetaModel(object):
     """
     Data for all components of this project.
-
     """
 
     def __init__(self):
@@ -17,55 +22,13 @@ class MetaModel(object):
         print("Loading config...")
         self.patient = Patient("Debra")
 
-        # DEMO VERSION -- NO DATABASE
-        """
-        with open('config.yaml', 'r') as f:
-            config = yaml.load(f, Loader=yaml.SafeLoader)
-
-        self.settings = config['settings']
-        print("Hashing password...")
-        self.settings['password'] = sha256(
-            self.settings['password'].encode('utf-8')).hexdigest()
-
-        self.config_teams = config['teams']
-        self.config_systems = config['systems']
-
-        print("Emptying existing database...")
-        db.reset_all_tables()
-
-        logging.info(
-            "------------------------ APP LAUNCHED ------------------------")
-        logging.info("Loading data into the in-memory model.")
-
-        # Initialize modules into the database
-        for module in modules.module_info.list_modules():
-            getattr(modules, module).create()
-
-        # Set MSFRPC current setting to off
-        self.msfrpc_loaded = 0
-        self.autostart = 0
-
-        # todo run through jobs
-        self.jobs = []
-
-        # Load global settings
-        self.run = 0
-        self.dest = 0
-        self.thread_lock = 0
-        self.msf_pw = "9wFZ8T7KWyydh2z4"
-
-        # Load teams and systems
-        self.teams = []
-        sorted_teams = sorted(self.config_teams.items(), key=lambda kv: kv[0])
-        for team in sorted_teams:
-            team_info = (team[1]['team_name'], team[1]['prefix'],
-                         team[1]['difficulty'])
-            self.write_team(team_info)
-        """
-
-
 class Patient(object):
-    def __init__(self, name):
+    def __init__(self, name):# pass in laterentered_patient_json = "data/patientData0.json"
+
+pj = get_json(entered_patient_json)
+
+        #
+
         self.name = name
         self.visits = [("Appointment at Sanford Health Center", "3PM Sunday", "ical", "maps"), \
         ("Chemotherapy at the Sanford Cancer Center", "1PM Monday", "ical", "maps")]
